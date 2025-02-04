06ea4a2040161483e8744b50b9aa6c5252362be1
chotmanat chanakul <mandalsunji@outlook.com>
2025-02-04 11:51:18 +0700

    create dashboard fix hydration error

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -50,10 +51,9 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
+        <Link
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
+          href="/dashboard"
           rel="noopener noreferrer"
         >
           <Image
@@ -64,7 +64,7 @@ export default function Home() {
             height={16}
           />
           Learn
-        </a>
+        </Link>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
