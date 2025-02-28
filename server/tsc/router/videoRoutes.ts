4c9598959c248b54c38a64b18bad45c375990a1c
คมสน คงผล <the78639@gmail.com>
2025-02-28 15:29:23 +0700

    add and fix add get medthod video fix wrong payload

--- server/tsc/router/videoRoutes.ts
+++ server/tsc/router/videoRoutes.ts
@@ -6,7 +6,34 @@ import { VehiclesSocket } from "../sockets/VehiclesSocket";
 import reqValidator from "../middlewares/reqValidator";
 import { createVideoSchema } from "../utils/validatorSchema";
 const router = Router();
+router.get("/api/video/all", [
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const video = await db2.video.findMany({ include: { vehicle_data: true } });
+			res.send({ data: video });
+		} catch (error) {
+			next(error);
+		}
+	},
+]);
+router.get("/api/video/:id", [
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { id } = req.params;
+			const video = await db2.video.findUnique({
+				where: { id: Number(id) },
+				include: { vehicle_data: true },
+			});
 
+			if (!video) {
+				throw new AppError("Not Found", 404);
+			}
+			res.send({ data: video });
+		} catch (error) {
+			next(error);
+		}
+	},
+]);
 router.post("/api/video", [
 	reqValidator(createVideoSchema),
 	async (req: Request, res: Response, next: NextFunction) => {
